7b111165082f64c48850733f00c1dfe495e9b5ae
iamthe-Wraith <allhailthewraith@protonmail.com>
2024-01-08 08:57:20 -0500

    chore: updates init colors and adds palette display page

---
+++ src/routes/css/colors/+page.svelte
@@ -0,0 +1,344 @@
+<section>
+    <p>light</p>
+
+    <div class="palette">
+        <div class="light-100">100</div>
+        <div class="light-200">200</div>
+        <div class="light-300">300</div>
+        <div class="light-400">400</div>
+        <div class="light-500">500</div>
+        <div class="light-600">600</div>
+        <div class="light-700">700</div>
+        <div class="light-800">800</div>
+        <div class="light-900">900</div>
+    </div>
+</section>
+
+<section>
+    <p>dark</p>
+
+    <div class="palette">
+        <div class="dark-100">100</div>
+        <div class="dark-200">200</div>
+        <div class="dark-300">300</div>
+        <div class="dark-400">400</div>
+        <div class="dark-500">500</div>
+        <div class="dark-600">600</div>
+        <div class="dark-700">700</div>
+        <div class="dark-800">800</div>
+        <div class="dark-900">900</div>
+    </div>
+</section>
+
+<section>
+    <p>primary</p>
+
+    <div class="palette">
+        <div class="primary-100">100</div>
+        <div class="primary-200">200</div>
+        <div class="primary-300">300</div>
+        <div class="primary-400">400</div>
+        <div class="primary-500">500</div>
+        <div class="primary-600">600</div>
+        <div class="primary-700">700</div>
+        <div class="primary-800">800</div>
+        <div class="primary-900">900</div>
+    </div>
+</section>
+
+<section>
+    <p>secondary</p>
+
+    <div class="palette">
+        <div class="secondary-100">100</div>
+        <div class="secondary-200">200</div>
+        <div class="secondary-300">300</div>
+        <div class="secondary-400">400</div>
+        <div class="secondary-500">500</div>
+        <div class="secondary-600">600</div>
+        <div class="secondary-700">700</div>
+        <div class="secondary-800">800</div>
+        <div class="secondary-900">900</div>
+    </div>
+</section>
+
+<section>
+    <p>tertiary</p>
+
+    <div class="palette">
+        <div class="tertiary-100">100</div>
+        <div class="tertiary-200">200</div>
+        <div class="tertiary-300">300</div>
+        <div class="tertiary-400">400</div>
+        <div class="tertiary-500">500</div>
+        <div class="tertiary-600">600</div>
+        <div class="tertiary-700">700</div>
+        <div class="tertiary-800">800</div>
+        <div class="tertiary-900">900</div>
+    </div>
+</section>
+
+<section>
+    <p>danger</p>
+
+    <div class="palette">
+        <div class="danger-100">100</div>
+        <div class="danger-200">200</div>
+        <div class="danger-300">300</div>
+        <div class="danger-400">400</div>
+        <div class="danger-500">500</div>
+        <div class="danger-600">600</div>
+        <div class="danger-700">700</div>
+        <div class="danger-800">800</div>
+        <div class="danger-900">900</div>
+    </div>
+</section>
+
+<style>
+    section {
+        margin-bottom: 2rem;
+    }
+
+    p {
+        margin: 0;
+    }
+
+    .palette {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        & div {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 3rem;
+            height: 3rem;
+        }
+    }
+
+    .light-100 {
+        background-color: var(--light-100);
+        color: #000;
+    }
+
+    .light-200 {
+        background-color: var(--light-200);
+        color: #000;
+    }
+
+    .light-300 {
+        background-color: var(--light-300);
+        color: #000;
+    }
+
+    .light-400 {
+        background-color: var(--light-400);
+        color: #000;
+    }
+
+    .light-500 {
+        background-color: var(--light-500);
+        color: #000;
+    }
+
+    .light-600 {
+        background-color: var(--light-600);
+        color: #000;
+    }
+
+    .light-700 {
+        background-color: var(--light-700);
+        color: #000;
+    }
+
+    .light-800 {
+        background-color: var(--light-800);
+        color: #000;
+    }
+
+    .light-900 {
+        background-color: var(--light-900);
+        color: #000;
+    }
+
+    .dark-100 {
+        background-color: var(--dark-100);
+    }
+
+    .dark-200 {
+        background-color: var(--dark-200);
+    }
+
+    .dark-300 {
+        background-color: var(--dark-300);
+    }
+
+    .dark-400 {
+        background-color: var(--dark-400);
+    }
+
+    .dark-500 {
+        background-color: var(--dark-500);
+    }
+
+    .dark-600 {
+        background-color: var(--dark-600);
+    }
+
+    .dark-700 {
+        background-color: var(--dark-700);
+    }
+
+    .dark-800 {
+        background-color: var(--dark-800);
+    }
+
+    .dark-900 {
+        background-color: var(--dark-900);
+    }
+
+    .primary-100 {
+        background-color: var(--primary-100);
+    }
+
+    .primary-200 {
+        background-color: var(--primary-200);
+    }
+
+    .primary-300 {
+        background-color: var(--primary-300);
+    }
+
+    .primary-400 {
+        background-color: var(--primary-400);
+    }
+
+    .primary-500 {
+        background-color: var(--primary-500);
+    }
+
+    .primary-600 {
+        background-color: var(--primary-600);
+    }
+
+    .primary-700 {
+        background-color: var(--primary-700);
+    }
+
+    .primary-800 {
+        background-color: var(--primary-800);
+    }
+
+    .primary-900 {
+        background-color: var(--primary-900);
+    }
+
+    .secondary-100 {
+        background-color: var(--secondary-100);
+    }
+
+    .secondary-200 {
+        background-color: var(--secondary-200);
+    }
+
+    .secondary-300 {
+        background-color: var(--secondary-300);
+    }
+
+    .secondary-400 {
+        background-color: var(--secondary-400);
+    }
+
+    .secondary-500 {
+        background-color: var(--secondary-500);
+    }
+
+    .secondary-600 {
+        background-color: var(--secondary-600);
+    }
+
+    .secondary-700 {
+        background-color: var(--secondary-700);
+    }
+
+    .secondary-800 {
+        background-color: var(--secondary-800);
+    }
+
+    .secondary-900 {
+        background-color: var(--secondary-900);
+    }
+
+    .tertiary-100 {
+        background-color: var(--tertiary-100);
+    }
+
+    .tertiary-200 {
+        background-color: var(--tertiary-200);
+    }
+
+    .tertiary-300 {
+        background-color: var(--tertiary-300);
+    }
+
+    .tertiary-400 {
+        background-color: var(--tertiary-400);
+    }
+
+    .tertiary-500 {
+        background-color: var(--tertiary-500);
+    }
+
+    .tertiary-600 {
+        background-color: var(--tertiary-600);
+    }
+
+    .tertiary-700 {
+        background-color: var(--tertiary-700);
+    }
+
+    .tertiary-800 {
+        background-color: var(--tertiary-800);
+    }
+
+    .tertiary-900 {
+        background-color: var(--tertiary-900);
+    }
+
+    .danger-100 {
+        background-color: var(--danger-100);
+    }
+
+    .danger-200 {
+        background-color: var(--danger-200);
+    }
+
+    .danger-300 {
+        background-color: var(--danger-300);
+    }
+
+    .danger-400 {
+        background-color: var(--danger-400);
+    }
+
+    .danger-500 {
+        background-color: var(--danger-500);
+    }
+
+    .danger-600 {
+        background-color: var(--danger-600);
+    }
+
+    .danger-700 {
+        background-color: var(--danger-700);
+    }
+
+    .danger-800 {
+        background-color: var(--danger-800);
+    }
+
+    .danger-900 {
+        background-color: var(--danger-900);
+    }
+</style>
