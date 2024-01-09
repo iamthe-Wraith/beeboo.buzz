038950cf366d7fa9e895585cfbc8aa6646a67aee
iamthe-Wraith <allhailthewraith@protonmail.com>
2024-01-09 10:07:24 -0500

    refactor: updates Sentry init with methods for applying values by env

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,21 +1,37 @@
-import { PUBLIC_SENTRY_DSN } from "$env/static/public";
+import { PUBLIC_APP_ENV, PUBLIC_SENTRY_DSN } from "$env/static/public";
+import { getTracesSampleRate } from "$lib/utils/sentry";
 import { handleErrorWithSentry, Replay } from "@sentry/sveltekit";
 import * as Sentry from '@sentry/sveltekit';
 
+const getIntegrations = () => {
+	if (PUBLIC_APP_ENV === 'production') return [new Replay()];
+	return [];
+};
+
+const getReplaysOnErrorSampleRate = () => {
+	return 1.0;
+};
+
+const getReplaysSessionSampleRate = () => {
+	if (PUBLIC_APP_ENV === 'production') return 0.1;
+	if (PUBLIC_APP_ENV === 'test') return 0;
+	return 1.0;
+};
+
 Sentry.init({
   dsn: PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 1.0,
+  tracesSampleRate: getTracesSampleRate(),
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: getReplaysSessionSampleRate(),
 
   // If the entire session is not sampled, use the below sample rate to sample
   // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+  replaysOnErrorSampleRate: getReplaysOnErrorSampleRate(),
   
   // If you don't want to use Session Replay, just remove the line below:
-  integrations: [new Replay()],
+  integrations: getIntegrations(),
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,19 @@
 import { sequence } from "@sveltejs/kit/hooks";
 import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
 import * as Sentry from '@sentry/sveltekit';
-import { PUBLIC_SENTRY_DSN } from "$env/static/public";
+import { PUBLIC_SENTRY_DSN, PUBLIC_APP_ENV } from "$env/static/public";
+import { getDenyUrls, getTracesSampleRate } from "$lib/utils/sentry";
 
 Sentry.init({
   dsn: PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 1.0,
+	environment: PUBLIC_APP_ENV,
+
+	// Sets a uniform sample rate for all transactions
+	tracesSampleRate: getTracesSampleRate(),
+
+	// ignore the following urls
+	// https://docs.sentry.io/platforms/javascript/guides/sveltekit/configuration/filtering/
+	denyUrls: getDenyUrls(),
 });
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
