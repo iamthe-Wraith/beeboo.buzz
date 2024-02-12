fc11276348dd3e513034214e2f74f068e9e27194
iamthe-Wraith <allhailthewraith@protonmail.com>
2024-02-11 21:32:02 -0500

    feat: adds checkmark icon to new Icon component

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -41,6 +41,25 @@
     </svg>
 {/if}
 
+{#if name === 'checkmark'}
+    <svg
+        xmlns="http://www.w3.org/2000/svg" 
+        width="1em" 
+        height="1em" 
+        viewBox="0 0 512 512" 
+        {...$$props}
+    >
+        <path 
+            fill="none" 
+            stroke="currentColor" 
+            stroke-linecap="round" 
+            stroke-linejoin="round" 
+            stroke-width="32" 
+            d="M416 128L192 384l-96-96"
+        />
+    </svg>
+{/if}
+
 {#if name === 'close'}
     <svg
         xmlns="http://www.w3.org/2000/svg" 
