1c5b995a0b467cf597948d26c1458104da7db549
Jake Lundberg <allhailthewraith@protonmail.com>
2024-03-20 09:00:23 -0400

    refactor: updates cache api to be vendor agnostic (#161)
    * refactor: updates cache api to be vendor agnostic
    
    * fix: removes returning early in cache when no redis URL found
    
    ioredis automatically connects to the local redis db when no url is
    provided. by returning early and not creating the redis client when no
    url was privided, it was causing the authentication hook to fail as no
    user was being returned.

--- src/lib/storage/cache.ts
+++ src/lib/storage/cache.ts
@@ -1,9 +1,39 @@
-import { Logger } from '$lib/services/logger';
 import Redis from 'ioredis';
 import { REDIS_URL } from '$env/static/private';
+import { Logger } from '$lib/services/logger';
 
-const redisClient = new Redis(REDIS_URL);
+class Cache {
+    private _client: Redis | null = null;
 
-redisClient.on('error', err => Logger.error('Cache Client Error', err));
+    constructor() {
+        this._client = new Redis(REDIS_URL);
 
-export const cache = redisClient;
+        this._client.on('error', (error) => {
+            Logger.error('Cache', error);
+        });
+    }
+
+    public del(key: string): Promise<number> {
+        if (!this._client) return Promise.resolve(0);
+
+        return this._client.del(key);
+    }
+
+    public get(key: string): Promise<string | null> {
+        if (!this._client) return Promise.resolve(null);
+
+        return this._client.get(key);
+    }
+
+    public set(key: string, value: string, expiration?: number): Promise<'OK' | null> {
+        if (!this._client) return Promise.resolve(null);
+
+        if (expiration) {
+            return this._client.set(key, value, 'EX', expiration);
+        }
+
+        return this._client.set(key, value);
+    }
+}
+
+export const cache = new Cache();
